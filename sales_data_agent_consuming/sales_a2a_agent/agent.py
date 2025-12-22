4528629d56b9395145927193d40d1ee18b63e84a
ahmadluay9 <ahmad.luay.adnani@gmail.com>
2025-12-22 07:34:26 +0000

    Add Vertex AI search functionality and enhance agent capabilities; update questions dataset

--- sales_data_agent_consuming/sales_a2a_agent/agent.py
+++ sales_data_agent_consuming/sales_a2a_agent/agent.py
@@ -3,6 +3,8 @@ import os
 from google.adk.agents.llm_agent import LlmAgent
 from google.adk.agents.remote_a2a_agent import AGENT_CARD_WELL_KNOWN_PATH
 from google.adk.agents.remote_a2a_agent import RemoteA2aAgent
+from google.adk.tools import VertexAiSearchTool
+from google.adk.tools.agent_tool import AgentTool
 from google.adk.models.google_llm import Gemini
 
 from google.genai.types import GenerateContentConfig
@@ -24,6 +26,38 @@ sales_agent = RemoteA2aAgent(
     agent_card=f"{REMOTE_AGENT_URL}{AGENT_CARD_WELL_KNOWN_PATH}",
 )
 
+vertexai_search_tool = VertexAiSearchTool(
+    data_store_id=f"projects/{os.getenv('GOOGLE_CLOUD_PROJECT')}/locations/global/collections/default_collection/dataStores/{os.getenv('DATASTORE_ID')}",
+    max_results=5
+)
+
+vertexai_search_agent = LlmAgent(
+    model=Gemini(
+        model=GEMINI_MODEL_FLASH,
+        retry_options=retry_config
+    ),
+    name='vertexai_search_agent',
+    description='A helpful assistant for answering questions based on documents retrieved from Vertex AI Search.',
+    instruction='Use the tool to answer user questions based on the retrieved documents.',
+    tools=[vertexai_search_tool],
+    generate_content_config=GenerateContentConfig(
+        temperature=0.1
+    )
+)
+
+rag_tool = AgentTool(agent=vertexai_search_agent, skip_summarization=False)
+
+rag_agent = LlmAgent(
+    model=Gemini(
+        model=GEMINI_MODEL_FLASH,
+        retry_options=retry_config
+    ),
+    name='sales_rag_agent_with_search',
+    description='A helpful assistant for answering sales-related questions using retrieved documents.',
+    instruction='Use the provided documents to answer user questions about sales data.',
+    tools=[rag_tool]
+)
+
 root_agent = LlmAgent(
     model=Gemini(
         model=GEMINI_MODEL_FLASH,
@@ -32,7 +66,7 @@ root_agent = LlmAgent(
     name='sales_a2a_agent',
     description='A helpful assistant for user questions.',
     instruction='Answer user questions to the best of your knowledge.',
-    sub_agents=[sales_agent],
+    sub_agents=[sales_agent, rag_agent],
     before_model_callback=normalize_user_query_guardrail,
     generate_content_config=GenerateContentConfig(
         temperature=0.1
